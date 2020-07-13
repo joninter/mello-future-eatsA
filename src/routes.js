0cc10fe54b999b28facb2a0df461a7294793bb5e
Jonatan Machado <jonatanmachado35@gmail.com>
2020-07-13 14:33:34 -0300

    fix: routes e initial apps

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
-import Login from './Pages/login';
+import Login from './pages/login';
 
 export default function Routes() {
   return (
